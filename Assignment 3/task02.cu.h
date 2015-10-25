9c1bda717d3b8f569f6c3fcff3de530d2e427802
Ssica <nyamo70@gmail.com>
2015-10-25 19:56:01 +0100

    plz

--- Assignment 3/task02.cu.h
+++ Assignment 3/task02.cu.h
@@ -14,7 +14,7 @@ __global__ void quick_2c_kernel(float* matrix_in, float* matrix_out, int row){
     float accum = matrix_in[i] * matrix_in[i];
     matrix_out[i+0] = accum;
     for(int k=1; k<64; k++){
-        float* tmpA = matrix_in[i+k];
+        float tmpA = matrix_in[i+k];
         accum = sqrt(accum)+tmpA * tmpA;
         matrix_out[i+k];
     }
@@ -25,7 +25,7 @@ __global__ void quick_2c_kernel(float* matrix_in, float* matrix_out, int row){
 2.d
 */
 
-__global__ void quick_2c_kernel(float* matrix_in, float* matrix_out, int row){
+__global__ void 2d_kernel(float* matrix_in, float* matrix_out, int row){
     int i = blockIdx.y * blockDim.y + threadIdx.y;
     //int j = blockIdx.x * blockDim.x + threadIdx.x;
     int col = 64;
