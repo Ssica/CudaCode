0a4c55ba2d3e25e9cfd4e80875d4070ec8f8d604
Ssica <nyamo70@gmail.com>
2015-10-25 20:31:49 +0100

    plz

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -7,14 +7,6 @@
 #define COLS 8192
 #define TILE 32
 
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1){
-	unsigned int resolution=1000000;
-	long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
-	result->tv_sec = diff / resolution;
-	result->tv_usec = diff % resolution;
-	return (diff<0);
-}
-
 int main(){
     size_t size = COLS * ROWS;
 	size_t mem_size = sizeof(float) * size;
