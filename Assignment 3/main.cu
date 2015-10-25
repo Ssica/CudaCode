8557cf67da682d1dcecfad4b7bf9468e9df1a7e4
Ssica <nyamo70@gmail.com>
2015-10-25 21:23:53 +0100

    plz

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -6,6 +6,13 @@
 #define ROWS 8192
 #define COLS 8192
 #define TILE 32
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1){
+	unsigned int resolution=1000000;
+	long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
+	result->tv_sec = diff / resolution;
+	result->tv_usec = diff % resolution;
+	return (diff<0);
+}
 
 
 void validate_trans(float* mat1, float* mat2){
@@ -81,5 +88,54 @@ int main(){
     printf("Tiled Parallel Transpose Kernel ran in %lu microseconds.\n",elapsed);
     cudaFree(d_A);    
     cudaFree(d_C);
+
+    //Task 2
+
+	cudaMalloc((void**)&d_A,mem_size);
+	cudaMalloc((void**)&d_C,mem_size);
+    cudaMemcpy(d_A, h_A, mem_size, cudaMemcpyHostToDevice);
+    gettimeofday(&t_start,NULL);
+    quick_2c_kernel(d_A, d_C, ROWS, COLS);
+	gettimeofday(&t_end, NULL);
+	timeval_subtract(&t_diff, &t_end, &t_start);
+	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
+    printf("2.c Kernel ran in %lu microseconds.\n",elapsed);
+    cudaFree(d_A);    
+    cudaFree(d_C);
+
+	cudaMalloc((void**)&d_A,mem_size);
+	cudaMalloc((void**)&d_C,mem_size);
+    cudaMemcpy(d_A, h_A, mem_size, cudaMemcpyHostToDevice);
+    gettimeofday(&t_start,NULL);
+    quick_2c_kernel(d_A, d_C, ROWS);
+	gettimeofday(&t_end, NULL);
+	timeval_subtract(&t_diff, &t_end, &t_start);
+	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
+    printf("2.d Kernel ran in %lu microseconds.\n",elapsed);
+    cudaFree(d_A);    
+    cudaFree(d_C);
+
+    //Matrix Multiplication
+
+	gettimeofday(&t_start, NULL);    
+    matrixmult_seq(h_A, h_A, h_B, ROWS, COLS, COLS);
+	gettimeofday(&t_end, NULL);
+	timeval_subtract(&t_diff, &t_end, &t_start);
+	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
+    printf("Sequential Matrix Multiplaction ran in %lu microseconds.\n",elapsed);
+
+    //Parallel Matrix Multiplication
+	cudaMalloc((void**)&d_A,mem_size);
+	cudaMalloc((void**)&d_C,mem_size);
+    cudaMemcpy(d_A, h_A, mem_size, cudaMemcpyHostToDevice);
+    gettimeofday(&t_start,NULL);
+    naive_matrixmult<T><<<grid,block>>>(d_A, d_A, d_C, ROWS, COLS, COLS);
+	gettimeofday(&t_end, NULL);
+	timeval_subtract(&t_diff, &t_end, &t_start);
+	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
+    printf("Naive Matrix Multiplication Kernel ran in %lu microseconds.\n",elapsed);
+    cudaFree(d_A);    
+    cudaFree(d_C);
+
 
 }
--- Assignment 3/seq.h
+++ Assignment 3/seq.h
@@ -2,15 +2,6 @@
 #include<stdlib.h>
 #include<sys/time.h>
 #include<time.h>
-
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1){
-	unsigned int resolution=1000000;
-	long int diff = (t2->tv_usec + resolution * t2->tv_sec) - (t1->tv_usec + resolution * t1->tv_sec);
-	result->tv_sec = diff / resolution;
-	result->tv_usec = diff % resolution;
-	return (diff<0);
-}
-
 /*
 1.a: Sequential implementation of tranpose
 */
