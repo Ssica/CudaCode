d6404d218480553f6f5021a2d921bb61a27f191d
Ssica <nyamo70@gmail.com>
2015-10-25 19:52:22 +0100

    plz

---
+++ Assignment 3/main.cu
@@ -0,0 +1,23 @@
+#include "task01.cu.h"
+#include "task02.cu.h"
+#include "task03.cu.h"
+#include "seq.h"
+
+#define ROWS 8192
+#define COLS 8192
+#define TILE 32
+
+int main(){
+size_t size = COLS * ROWS;
+	size_t mem_size = sizeof(float) * size;
+	float* h_A = (float*) malloc(mem_size);
+	float* h_B = (float*) malloc(mem_size);
+	float* h_C = (float*) malloc(mem_size);
+
+    float* d_A;
+	float* d_C;
+	cudaMalloc((void**)&d_A,mem_size);
+	cudaMalloc((void**)&d_C,mem_size);
+
+    init_matrix(h_a, size);
+}
