fa6e48242030551ac5bb9cbae5a97dc638865de7
Ssica <nyamo70@gmail.com>
2015-10-25 20:43:35 +0100

    plz

--- Assignment 3/main.cu
+++ Assignment 3/main.cu
@@ -39,7 +39,7 @@ int main(){
 
     cudaMemcpy(d_A, h_A, mem_size, cudaMemcpyHostToDevice);
     gettimeofday(&t_start,NULL);
-    transpose_kernel_naive<T, TILE>(float* d_A, float* d_C, ROWS, COLS);
+    transpose_kernel_naive<float, TILE>(float* d_A, float* d_C, ROWS, COLS);
 	gettimeofday(&t_end, NULL);
 	timeval_subtract(&t_diff, &t_end, &t_start);
 	elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec);
